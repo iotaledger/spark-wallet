565d41260d52f3e14594914c169965ff8c90419e
Rihards Gravis <rihards.gravis@iota.org>
2019-12-12 12:39:44 +0200

    - Update dependencies - Add static manifest.json fallback

--- index.ts
+++ index.ts
@@ -9,14 +9,14 @@ const app = express()
 const config = require('./webpack.config.js')
 const compiler = webpack(config)
 
+app.use('/api/time', routeTime)
+app.use('/manifest.json', routeManifest)
+
 app.use(
     webpackDevMiddleware(compiler, {
         publicPath: config.output.publicPath
     })
 )
-
-app.use('/api/time', routeTime)
-app.use('/manifest.json', routeManifest)
 
 app.listen(3000, function() {
     console.log('Spark running on port 3000')
